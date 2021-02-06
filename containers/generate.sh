70159e096589fe1b823546d26db30d9223c909ad
Nathan DeMasie <nathan.demasie@gmail.com>
2021-02-06 15:50:01 +0100

    styling and fix broken loop

--- containers/generate.sh
+++ containers/generate.sh
@@ -5,17 +5,18 @@ export TZ
 
 env_file="${CURDIR}/.env.prod"
 [ ! -f $env_file ] && env_file="${CURDIR}/.env"
-[ ! -f $env_file ] && echo "${red}ERROR: No \".env\" file found${reset}" && exit 1
+[ ! -f $env_file ] && echo "${red}ERROR${reset}: No \".env\" file found" && exit 1
 
 save_selections_file="${CURDIR}/.save.selections"
-[ ! -f $save_selections_file ] && echo "No \".save\" file found. Sending to menu..." && bash ${CURDIR}/menu.sh
+[ ! -f $save_selections_file ] && echo "${red}ERROR${reset}: No selections found. Sending to menu..." && bash ${CURDIR}/menu.sh
 
 readarray -t selections < $save_selections_file
 
-for container in $selections; do
-  files+="-f ${CURDIR}/${container}/docker-compose.yml"
+for container in ${selections[@]}; do
+  compose_files+="-f ${CURDIR}/${container}/docker-compose.yml"
 done
 
-cmd=$"docker-compose --env-file $env_file $files"
-echo $cmd > "${CURDIR}/.cmd.docker-compose"
+cmd=$"docker-compose --env-file $env_file $compose_files"
 $cmd config | grep --silent "DEBUG|INFO|WARNING|ERROR|CRITICAL"
+
+printf '%s' "$cmd" > "${CURDIR}/.cmd.docker-compose"
--- containers/run.sh
+++ containers/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 CURDIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")") # Sets current directory agnostic of run location
-echo "$(< ${CURDIR}/.cmd.docker-compose) up -d"
+
+compose_file="${CURDIR}/.cmd.docker-compose"
+[ ! -f $compose_file ] && ${CURDIR}/generate.sh
+
+echo "$(< $compose_file) up -d"
