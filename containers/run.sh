a65d7c2e313820e8829e086aae47eb7339c2959c
Nathan DeMasie <nathan.demasie@gmail.com>
2021-02-06 19:04:13 +0100

    export env when running

--- containers/run.sh
+++ containers/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-CURDIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")") # Sets current directory agnostic of run location
+export CURDIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")") # Sets current directory agnostic of run location
+source $(dirname "$CURDIR")/helpers/variables.sh
+export TZ=${TZ}
 
 compose_file="${CURDIR}/.tmp/.cmd.docker-compose"
 [ -f $compose_file ] || ${CURDIR}/generate.sh
