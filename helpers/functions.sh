9ad80662f07cae56ada4914e10c1d6e27506fb9b
Nathan DeMasie <nathan.demasie@gmail.com>
2021-02-14 16:06:11 +0100

    add function argument passing

--- helpers/functions.sh
+++ helpers/functions.sh
@@ -2,19 +2,34 @@
 source $(dirname -- "$(readlink -f -- "$BASH_SOURCE")")/variables.sh
 
 function ensure_path() {
-  local file=${1}
-  path=${file%/*}
-  [ ! -f $file ] && mkdir -p $path && touch -a $file
+  local filepath=${1}
+  local dirpath=${file%/*}
+  [ ! -f $filepath ] && mkdir -p $dirpath && touch -a $filepath
 }
 
 function has_command() {
-  local cmd=$@
+  local cmd=${1}
   command -v "$cmd" >/dev/null 2>&1
 }
 
 function execute() {
-  local path=${1}
-  [ ! -e $path ] && printf "%s\n" "${red}ERROR${reset}: $path not found" && return 1
+  local path
+  local quiet=1 # false
+
+  # https://medium.com/@Drew_Stokes/bash-argument-parsing-54f3b81a6a8f
+  while (( "$#" )); do
+    case "$1" in
+      -q|--quiet) quiet=0 && shift ;;
+      -*|--*=) echo "${red}Error${reset}: Unsupported flag $1" >&2 && shift ;;
+      *) path=$1 && shift ;;
+    esac
+  done
+
+  if [ ! -e $path ]; then
+    [ $quiet -eq 0 ] || printf "%s\n" "${red}ERROR${reset}: $path not found"
+    return 1
+  fi
+
   [ -x $path ] || sudo chmod +x $path
   bash $path
 }
