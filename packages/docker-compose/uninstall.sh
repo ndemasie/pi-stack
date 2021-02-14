fd915ad2e9b923c37a3cbb84a2127449192f371a
Nathan DeMasie <nathan.demasie@gmail.com>
2021-02-14 21:25:40 +0100

    revert docker-compose install change due to old apt package

--- packages/docker-compose/uninstall.sh
+++ packages/docker-compose/uninstall.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # https://phoenixnap.com/kb/install-docker-compose-on-ubuntu-20-04
 
-echo "Uninstalling docker-compose"
-sudo rm /usr/local/bin/docker-compose
-sudo apt remove docker-compose
-sudo apt autoremove
+# echo "Uninstalling docker-compose"
+sudo pip3 uninstall docker-compose -y
