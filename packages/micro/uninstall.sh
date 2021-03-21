77f4ca8cb588b1a2138436b3ee487de915fde8df
Nathan DeMasie <nathan.demasie@gmail.com>
2021-03-21 19:08:44 +0100

    fix bugs in uninstall script

--- packages/micro/uninstall.sh
+++ packages/micro/uninstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Uninstalling micro..."
-[ -d  /usr/bin/micro ] sudo rm /usr/bin/micro
+if [ -f  /usr/bin/micro ]; then sudo rm /usr/bin/micro; fi
+if [ -d  ~/.config/micro ]; then sudo rm -r ~/.config/micro; fi
 
 echo "micro was uninstalled"
