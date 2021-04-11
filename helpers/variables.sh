b4693ab1bf054c4f4b8fda1949ee834c296a3a54
Nathan DeMasie <nathan.demasie@gmail.com>
2021-04-11 07:34:45 +0200

    add SGR output styles

--- helpers/variables.sh
+++ helpers/variables.sh
@@ -7,24 +7,28 @@ TZ=$(cat /etc/timezone) # Timezone
 
 if [ -z ${NO_COLOR} -o -z ${NOCOLOR} ]; then
 	# ANSI COLORS: https://en.wikipedia.org/wiki/ANSI_escape_code
-	BLACK=$'\e[0;30m'
-	RED=$'\e[0;31m'
-	GREEN=$'\e[0;32m'
+	BLACK=$'\e[30m'
+	RED=$'\e[31m'
+	GREEN=$'\e[32m'
 	YELLOW=$'\e[01;33m'
-	BLUE=$'\e[0;34m'
-	MAGENTA=$'\e[0;35m'
-	CYAN=$'\e[0;36m'
-	WHITE=$'\e[0;37m'
+	BLUE=$'\e[34m'
+	MAGENTA=$'\e[35m'
+	CYAN=$'\e[36m'
+	WHITE=$'\e[37m'
 
-	BRIGHT_BLACK=$'\e[0;90m'
-	BRIGHT_RED=$'\e[0;91m'
-	BRIGHT_GREEN=$'\e[0;92m'
+	BRIGHT_BLACK=$'\e[90m'
+	BRIGHT_RED=$'\e[91m'
+	BRIGHT_GREEN=$'\e[92m'
 	BRIGHT_YELLOW=$'\e[01933m'
-	BRIGHT_BLUE=$'\e[0;94m'
-	BRIGHT_MAGENTA=$'\e[0;95m'
-	BRIGHT_CYAN=$'\e[0;96m'
-	BRIGHT_WHITE=$'\e[0;97m'
+	BRIGHT_BLUE=$'\e[94m'
+	BRIGHT_MAGENTA=$'\e[95m'
+	BRIGHT_CYAN=$'\e[96m'
+	BRIGHT_WHITE=$'\e[97m'
 
-  BOLD=$(tput bold)
+	# SGR (Select Graphic Rendition) parameters
   NC=$'\e[0m'
+  BOLD=$'\e[1m'
+	FAINT=$'\e[2m'
+	ITALIC=$'\e[3m'
+	UNDERLINE=$'\e[4m'
 fi
