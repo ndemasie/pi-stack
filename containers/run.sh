ba81d2a3ae5284e3b6c742d91b14c1a17842a2a7
Nathan DeMasie <nathan.demasie@gmail.com>
2021-04-17 16:01:51 +0200

    feat: Containers run script use passable docker-compose arg

--- containers/run.sh
+++ containers/run.sh
@@ -2,6 +2,8 @@
 CURDIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")") # Sets current directory agnostic of run location
 source $(dirname "$CURDIR")/helpers/variables.sh
 
+SCRIPT_RUN_CMD=${1,,}
+
 STATE_SELECTIONS_PATH="${CURDIR}/.state.selections"
 readarray -t state_selections < $STATE_SELECTIONS_PATH
 
@@ -9,5 +11,11 @@ for container in ${state_selections[@]}; do
   compose_files+=" --file ${CURDIR}/${container}/docker-compose.yml"
 done
 
-# sudo docker-compose $compose_files config
-sudo docker-compose $compose_files up --detach
+case $SCRIPT_RUN_CMD in
+  c|config) compose_cmd="config";;
+  d|down) compose_cmd="down";;
+  u|up) compose_cmd="up --detach";;
+  *) echo "${RED}ERROR${RESET}: Script docker-compose command '$SCRIPT_RUN_CMD' not recognized" && exit 1;;
+esac
+
+sudo docker-compose $compose_files $compose_cmd
