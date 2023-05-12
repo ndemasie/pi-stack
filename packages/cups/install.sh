687288b75a8cde3518337944bb5fbb4101e5f04a
ndemasie <nathan.demasie@gmail.com>
2023-05-12 22:23:28 +0200

    fix(package): fix some CUP install steps

--- packages/cups/install.sh
+++ packages/cups/install.sh
@@ -8,9 +8,13 @@ sudo apt install -y avahi-daemon
 echo "${name} dependencies installed"
 
 echo "Enable web interface on port ${port}"
-sudo sed -i "s/Listen localhost:${port}/Port ${port}/" /etc/cups/cupsd.conf
+sudo sed -i "/Listen localhost:631/i Port ${port}" /etc/cups/cupsd.conf
+sudo sed -i "/Listen localhost:631/i Listen Port ${port}/" /etc/cups/cupsd.conf
 sudo sed -i '/DefaultAuthType Basic/a DefaultEncryption Never' /etc/cups/cupsd.conf
-sudo sed -i '/<\/Location>/i Allow @local' /etc/cups/cupsd.conf
+sudo sed -i '/<\/Location>/i   Allow @local' /etc/cups/cupsd.conf
+
+echo "Update group for user"
+sudo usermod -a -G lpadmin $USER
 
 echo "Spinning up ${name} on port ${port}"
 sudo service cups restart
