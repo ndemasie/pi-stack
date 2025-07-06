a75f4b62c11dfbca5a8604f31da88e973e8b688f
ndemasie <nathan.demasie@gmail.com>
2025-07-06 11:23:29 -0400

    white

--- dashboard/main.py
+++ dashboard/main.py
@@ -22,13 +22,15 @@ class MonitorApp:
 
     def setup_curses(self) -> None:
         curses.curs_set(0)  # Hide cursor
-        curses.init_pair(0, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
         curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
         curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)
         curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
         curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
         curses.init_pair(5, curses.COLOR_RED, curses.COLOR_BLACK)
         curses.init_pair(6, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+
+        curses.init_pair(10, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     def update(self, time: float = time.time()) -> None:
         self.hardware_widget.update(time)
